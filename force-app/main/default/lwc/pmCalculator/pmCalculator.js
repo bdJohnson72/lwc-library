295924889e9808b9b1a7bc96ad73fc6d7bd69f0e
Brooks Johnson <brooksjohnson157@gmail.com>
2020-02-10 07:41:11 -0500

    added css

--- force-app/main/default/lwc/pmCalculator/pmCalculator.js
+++ force-app/main/default/lwc/pmCalculator/pmCalculator.js
@@ -6,28 +6,38 @@ import { LightningElement, track } from 'lwc';
 
 export default class PmCalculator extends LightningElement {
 @track mostLikely;
-@track leastLikely;
-@track bestCase;
+@track pessimistic;
+@track optimistic;
 @track estimate = 0.0;
 
 handleChange(event){
     console.log(event.target.name);
     console.log(event.target.value);
-    if (event.target.name){}
+    if (event.target.name === "mostLikely"){
+        this.mostLikely = event.target.value;
+        console.log(this.mostLikely)
+    }else if (event.target.name === "pessimistic"){
+        this.pessimistic = event.target.value;
+        console.log(this.pessimistic);
+    }else {
+        this.optimistic = event.target.value;
+        console.log(this.optimistic);
+    }
 }
 
 calculate(){
+    this.estimate = parseFloat((this.optimistic + (4*this.mostLikely) + this.pessimistic) / 6);
+    console.log(this.optimistic);
+    console.log(this.mostLikely * 4);
+    console.log(this.pessimistic);
+    console.log(this.estimate / 6);
 
 }
 clear(){
     this.mostLikely = 0.0;
-    this.leastLikely = 0.0;
-    this.bestCase = 0.0;
+    this.pessimistic = 0.0;
+    this.optimistic = 0.0;
     this.estimate = 0.0;
-}
-renderedCallback() {
-    //super.renderedCallback();
-    //this.template.querySelector(`[data-id="${worstCase}"]`).focus()
 }
 
 }
